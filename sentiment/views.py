1626aff926bfa120b804a89198129bffe37d2ca8
Asya <111695385+asyanix@users.noreply.github.com>
2025-05-02 18:19:17 +0300

    Added buttons to the website to upload files for big data processing

--- sentiment/views.py
+++ sentiment/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import tokenizer_from_json
+from django.http import FileResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.core.files.storage import default_storage
+from django.conf import settings
+import os
 import json
 import os
 
@@ -35,3 +40,29 @@ def index(request):
             result = sentiment[prediction.argmax()]  # Определяем тональность
 
     return render(request, 'sentiment/index.html', {'result': result})
+
+
+def create_report(csv_path, docx_path):
+    from docx import Document
+    doc = Document()
+    doc.add_heading('Отчет по анализу тональности', 0)
+    doc.add_paragraph('Здесь будет содержаться результат анализа файла CSV.')
+    doc.save(docx_path)
+
+def upload_csv(request):
+    if request.method == 'POST' and request.FILES.get('csv_file'):
+        csv_file = request.FILES['csv_file']
+        filename = default_storage.save('temp_uploaded.csv', csv_file)
+        csv_path = os.path.join(settings.MEDIA_ROOT, filename)
+
+        report_path = os.path.join(settings.MEDIA_ROOT, 'report.docx')
+
+        create_report(csv_path, report_path)
+
+        report_url = settings.MEDIA_URL + 'report.docx'
+        return render(request, 'sentiment/index.html', {
+            'report_generated': True,
+            'report_url': report_url
+        })
+
+    return HttpResponseRedirect(reverse('index'))
